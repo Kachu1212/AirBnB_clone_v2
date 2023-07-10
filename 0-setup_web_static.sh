e29c1ce2bf07a1f972ead6c99acf59dca01460b5
Nwosu Chukwuemeka K <78231106+Kachu1212@users.noreply.github.com>
2023-07-10 12:57:45 +0100

    Updated 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,7 +10,6 @@ echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu:ubuntu /data/
-chgrp -R ubuntu:ubuntu /data/
 
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
