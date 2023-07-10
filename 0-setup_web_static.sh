9be16f3694c9c75be9c9e88ea8e0cee9b7e76fd8
Nwosu Chukwuemeka K <78231106+Kachu1212@users.noreply.github.com>
2023-07-10 12:34:35 +0100

    Updated 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,4 +14,3 @@ chgrp -R ubuntu:ubuntu /data/
 
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
-exit 0
