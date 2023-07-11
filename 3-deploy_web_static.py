1672cf93374b820e1ce8a8ad45aad2754fd5fc92
Nwosu Chukwuemeka K <78231106+Kachu1212@users.noreply.github.com>
2023-07-11 12:51:38 +0100

    Updated 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['3.85.54.88', '54.237.125.19']
 
 
 def do_pack():
